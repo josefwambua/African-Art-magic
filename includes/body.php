524203a85abe102097e4abcbdc7e7ccda6ba162a
Joseph Wambua <josefwambua2@gmail.com>
2024-10-05 18:48:47 +0300

    grid

--- includes/body.php
+++ includes/body.php
@@ -29,43 +29,7 @@
       </div>
     </div>
   </div>
-  <!-- <div class="col"> -->
-  <div class="col-lg-4 col-md-4 col-sm-6">
-    <div class="card">
-      <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/043.webp" class="card-img-top" alt="Los Angeles Skyscrapers">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
-          additional content. This content is a little bit longer.</p>
-      </div>
-    </div>
-  </div>
-  <!-- <div class="col"> -->
-  <div class="col-lg-4 col-md-4 col-sm-6">
-    <div class="card">
-      <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/044.webp" class="card-img-top" alt="Skyscrapers">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">
-          This is a longer card with supporting text below as a natural lead-in to
-          additional content. This content is a little bit longer.
-        </p>
-      </div>
-    </div>
-  </div>
-  <!-- <div class="col"> -->
-  <div class="col-lg-4 col-md-4 col-sm-6">
-    <div class="card">
-      <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/046.webp" class="card-img-top" alt="Skyscrapers">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">
-          This is a longer card with supporting text below as a natural lead-in to
-          additional content. This content is a little bit longer.
-        </p>
-      </div>
-    </div>
-  </div>
+ 
   <!-- <div class="col">
      -->
      <div class="col-lg-4 col-md-4 col-sm-6">
